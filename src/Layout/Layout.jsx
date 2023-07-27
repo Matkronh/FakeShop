b12670e8ec4f39f38d5667d87aa147331fef0285
matkronh <mats.kodehode@gmail.com>
2023-07-27 10:10:50 +0200

    d

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -35,6 +35,34 @@ function Layout() {
                 <div className={styles.header}>
                     <h1 className={styles.headerText}>Louis Betong</h1>
                 </div>
+
+                <div className={styles.routebar}>
+                    <div className={styles.subroutebar1}>
+                        <div className={styles.routerposition}>
+                            <div className={styles.subrouterposition}>
+                                <h1 className={styles.routerText}>Hats</h1>
+                            </div>
+                        </div>
+                        <div className={styles.routerposition}>
+                            <div className={styles.subrouterposition}>
+                                <h1 className={styles.routerText}>Shirts</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.subroutebar2}>
+                        <div className={styles.routerposition}>
+                        <div className={styles.subrouterposition}>
+                                <h1 className={styles.routerText}>Pants</h1>
+                            </div>
+                        </div>
+                        <div className={styles.routerposition}>
+                        <div className={styles.subrouterposition}>
+                                <h1 className={styles.routerText}>Shoes</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
     
                 <Outlet/>
 
